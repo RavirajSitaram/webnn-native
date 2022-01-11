b43c402d46a3e1ae21390c3e13f53e93a90d733b
Raviraj P Sitaram <raviraj.p.sitaram@intel.com>
2022-01-11 13:59:19 +0530

    Add Backend class for Nnapi
    Signed-off-by: Raviraj P Sitaram <raviraj.p.sitaram@intel.com>

--- src/webnn_native/Instance.cpp
+++ src/webnn_native/Instance.cpp
@@ -58,6 +58,11 @@ namespace webnn_native {
         BackendConnection* Connect(InstanceBase* instance);
     }
 #endif  // defined(WEBNN_ENABLE_BACKEND_XNNPACK)
+#if defined(WEBNN_ENABLE_BACKEND_NNAPI)
+    namespace nnapi {
+        BackendConnection* Connect(InstanceBase* instance);
+    }
+#endif  // defined(WEBNN_ENABLE_BACKEND_NNAPI)
 
     namespace {
 
@@ -81,6 +86,9 @@ namespace webnn_native {
 #if defined(WEBNN_ENABLE_BACKEND_XNNPACK)
             enabledBackends.set(wnn::BackendType::XNNPACK);
 #endif  // defined(WEBNN_ENABLE_BACKEND_XNNPACK)
+#if defined(WEBNN_ENABLE_BACKEND_NNAPI)
+            enabledBackends.set(wnn::BackendType::Nnapi);
+#endif  // defined(WEBNN_ENABLE_BACKEND_NNAPI)
             return enabledBackends;
         }
 
@@ -151,6 +159,11 @@ namespace webnn_native {
                 break;
 #endif  // defined(WEBNN_ENABLE_BACKEND_XNNPACK)
 
+#if defined(WEBNN_ENABLE_BACKEND_NNAPI)
+            case wnn::BackendType::Nnapi:
+                Register(nnapi::Connect(this), wnn::BackendType::Nnapi);
+                break;
+#endif  // defined(WEBNN_ENABLE_BACKEND_NNAPI)
             default:
                 UNREACHABLE();
         }
@@ -172,6 +185,8 @@ namespace webnn_native {
             return mBackends[wnn::BackendType::MLAS]->CreateContext(options);
         } else if (mBackends.find(wnn::BackendType::XNNPACK) != mBackends.end()) {
             return mBackends[wnn::BackendType::XNNPACK]->CreateContext(options);
+        } else if (mBackends.find(wnn::BackendType::Nnapi) != mBackends.end()) {
+            return mBackends[wnn::BackendType::Nnapi]->CreateContext(options);
         }
         UNREACHABLE();
         return nullptr;
